de7973ab65159320336ebe6b5a099892ec056b0c
chriscortes-dev <crist.cortes5@gmail.com>
2026-03-25 21:54:53 -0300

    Registra modelos en admin para visualización

--- mascota/admin.py
+++ mascota/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(EspecieMascota)
+admin.site.register(RazaMascota)
+admin.site.register(Mascota)
+admin.site.register(HistorialMedicoMascota)
+admin.site.register(EstadoMascota)
+admin.site.register(UbicacionMascota)
